e30a08b50a3c396b15deee1428c5c6cb3c3a4358
Muhammad Ashraf <muhammad.a.hassan.0@gmail.com>
2021-04-06 09:04:33 +0200

    Requirements

---
+++ xray_api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class XRay(Resource):
+    def get(self):
+        data = "1"
+        return {'output': data}, 200
+
+    pass
+
+api.add_resource(XRay, '/xray')
+
+if __name__ == '__main__':
+    app.run(debug = False, host='0.0.0.0')
