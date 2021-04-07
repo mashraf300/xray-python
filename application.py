0d9b31e1e1400b5adf5508ee21c91dd5b39ccfad
Muhammad Ashraf <muhammad.a.hassan.0@gmail.com>
2021-04-07 10:58:31 +0200

    Post Request

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 import ast
 import numpy as np
@@ -6,6 +6,8 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 
+UPLOAD_FOLDER = '/test/NORMAL'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 try:
     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
@@ -46,32 +48,41 @@ def process_path(file_path):
 
 
 application = Flask(__name__)
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 api = Api(application)
 
+@application.route('/xray', methods = ['POST'])
+def upload_file():
+    if 'image' not in request.files:
+            return {'output': -1}, 200
 
-class XRay(Resource):
-    def get(self):
-        data = "1"
-        test_list_ds = tf.data.Dataset.list_files(str('test/*/*'))
-        TEST_IMAGE_COUNT = tf.data.experimental.cardinality(test_list_ds).numpy()
+    img = request.files['image']
 
-        print(TEST_IMAGE_COUNT)
+    print(img.filename)
+
+    filename = img.filename
+    img.save("test/NORMAL/image.jpg")
+
+    test_list_ds = tf.data.Dataset.list_files(str('test/*/*'))
+    TEST_IMAGE_COUNT = tf.data.experimental.cardinality(test_list_ds).numpy()
+
+    print(TEST_IMAGE_COUNT)
 
 
-        test_ds = test_list_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        test_ds = test_ds.batch(BATCH_SIZE)
+    test_ds = test_list_ds.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    test_ds = test_ds.batch(BATCH_SIZE)
 
 
-        reconstructed_model = keras.models.load_model("xray_model.h5")
+    reconstructed_model = keras.models.load_model("xray_model.h5")
 
-        y = reconstructed_model.predict_classes(test_ds)
+    y = reconstructed_model.predict_classes(test_ds)
 
-        print(y[0][0])
-        return {'output': int(y[0][0])}, 200
+    print(y[0][0])
 
-    pass
+    os.remove("test/NORMAL/image.jpg")
+    
+    return {'output': int(y[0][0])}, 200
 
-api.add_resource(XRay, '/xray')
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=5000, debug=True)
+    application.run(host='127.0.0.1', port=5000, debug=True)
